a97062e53d5f1daa018ec77be4b2971bae5a5c3e
Harsh Singhal <harshsinghal8899@gmail.com>
2026-04-03 22:18:11 +0530

    removed duplicate and wrong folders

--- backened/routes/useroute.js
+++
@@ -1,7 +0,0 @@
-import express from "express"
-import { SignUp,Login,Logout } from "../controllers/auth.js"
-const authRouter=express.Router()
-authRouter.post("/signup",SignUp)
-authRouter.post("/signin",Login)
-authRouter.get("/logout",Logout)
-export default authRouter;
